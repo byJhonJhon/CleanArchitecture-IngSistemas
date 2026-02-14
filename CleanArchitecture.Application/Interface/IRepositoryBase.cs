7fdcc5363083ed71ff917225cccef6198935c171
byJhonJhon <jhon_maniacthrash@hotmail.com>
2026-02-14 12:00:17 -0500

    Agregar archivos de proyecto.

---
+++ CleanArchitecture.Application/Interface/IRepositoryBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Application.Interface
+{
+    // Interfaz genérica para repositorios, define las operaciones CRUD básicas
+    public interface IRepositoryBase<T> where T : class
+    {
+        T Get(int id);
+        IEnumerable<T> GetAll();
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(int id);
+    }
+}
---
+++ CleanArchitecture.Infrastructure/ConfigurationServices.cs
@@ -0,0 +1,21 @@
+﻿using CleanArchitecture.Application.Interface;
+using CleanArchitecture.Infrastructure.Data.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitecture.Infrastructure
+{
+    public static class ConfigurationServices
+    {
+        public static void AddInfrastructureServices(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IRepositoryBase<>), typeof(RepositoryBase<>));
+            services.AddScoped<IAlmacenRepository, AlmacenRepository>();
+            services.AddScoped<ICategoriaRepository, CategoriaRepository>();
+            services.AddScoped<IExistenciaRepository, ExistenciaRepository>();
+            services.AddScoped<IUnidadMedidaRepository, UnidadMedidaRepository>();
+            services.AddScoped<IMovimientoInventarioRepository, MovimientoInventarioRepository>();
+            services.AddScoped<IProductoRepository, ProductoRepository>();
+            services.AddScoped<IProveedorRepository, ProveedorRepository>();
+        }
+    }
+}
